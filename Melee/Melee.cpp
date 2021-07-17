ef95a4dc87ba39dc2c7a8b040cf27ebcca8bdcc3
Dean Camera <dean@fourwalledcubicle.com>
2021-07-17 15:33:05 +1000

    Set up basic SDL application.

---
+++ Melee/Melee.cpp
@@ -0,0 +1,27 @@
+﻿#include "SDL.h"
+
+int main(int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Melee",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        1024,
+        768,
+        SDL_WINDOW_FULLSCREEN
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
